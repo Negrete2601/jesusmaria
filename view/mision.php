8d27916c4c8e44245cf42f7c10445e75f87aed9d
Johana negrete <130407370+Negrete2601@users.noreply.github.com>
2024-09-27 08:55:45 -0600

    mision y vision

---
+++ view/mision.php
@@ -0,0 +1,54 @@
+
+
+        <!-- Modal Search Start -->
+        <div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-fullscreen">
+                <div class="modal-content rounded-0">
+                    <div class="modal-header">
+                        <h4 class="modal-title text-secondary mb-0" id="exampleModalLabel">Search by keyword</h4>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body d-flex align-items-center">
+                        <div class="input-group w-75 mx-auto d-flex">
+                            <input type="search" class="form-control p-3" placeholder="keywords" aria-describedby="search-icon-1">
+                            <span id="search-icon-1" class="input-group-text p-3"><i class="fa fa-search"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Modal Search End -->
+
+        <!-- Header Start -->
+        <div class="container-fluid bg-breadcrumb2">
+            <div class="container text-center py-5" style="max-width: 900px;">
+                <h3 class="text-white display-3 mb-4 wow fadeInDown" data-wow-delay="0.1s">Misión</h1>
+                <ol class="breadcrumb justify-content-center text-white mb-0 wow fadeInDown" data-wow-delay="0.3s">
+                    <li class="breadcrumb-item"><a href="index.html" class="text-white">Inicio</a></li>
+                    <li class="breadcrumb-item"><a href="#" class="text-white">¡Mi orgullo, mi gente!</a></li>
+                    <li class="breadcrumb-item active text-secondary">Misión</li>
+                </ol>    
+            </div>
+        </div>
+        <!-- Header End -->
+
+       
+
+        
+
+        <!-- Countries We Offer Start -->
+        <div class="container-fluid country overflow-hidden py-5">
+            <div class="container py-5">
+                <div class="section-title text-center wow fadeInUp" data-wow-delay="0.1s" style="margin-bottom: 70px;">
+                    <div>
+                        <div style="text-align:justify">
+		
+		                 <p><br>
+		                    <span>Hacer de Jesús María, un municipio ordenado, generoso y ejemplar, en el que prevalezca una convivencia respetuosa, pacífica, orientada a la armonía; que promueva la prosperidad de las personas y familias que lo habitan, en el que las empresas puedan desarrollarse en un entorno productivo, competitivo, libre de corrupción y en el que las personas, sin distinción de origen étnico, credo, sexo, edad, los tipos de familia en los que vivan, su manera de vestir, hablar u otras características, puedan participar de un modelo de desarrollo humano sustentable, incluyente, al servicio de todas las personas.</span></p>
+	                     </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <!-- Countries We Offer End -->
