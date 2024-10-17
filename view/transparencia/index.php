f87575a6df2cf23a453b08fe8d92355c7c4c4893
Johana negrete <130407370+Negrete2601@users.noreply.github.com>
2024-10-17 13:06:57 -0600

    transparencia

--- view/transparencia/index.php
+++ view/transparencia/index.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8">
-        <title>xPresidencia municipal de Jesús María</title>
+        <title>Presidencia municipal de Jesús María</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="" name="keywords">
         <meta content="" name="description">
