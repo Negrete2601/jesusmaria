6a6750364ccbda1f55a51bb2d95f81ef7ac2a839
Jose Manuel <31418107+hosce@users.noreply.github.com>
2025-11-13 14:06:27 -0600

    Hotfix funciones tramites
    Modifica funciones de los tramites

--- controller/dependencias/funciones_dependencias.php
+++ controller/dependencias/funciones_dependencias.php
@@ -25,7 +25,7 @@ function get_dependencias()
 function get_tramites_dependenciat($id_dependencia)
 {
 	
-	$sql ="SELECT t.id, t.nombre, s.secretaria, t.documento_obtenido, t.telefono
+	$sql ="SELECT t.id, t.nombre, t.clave, t.usuarios, t.costo, t.tiempo, t.vigencia, t.oficina, t.domicilio, t.horario, t.resolucion, t.email, s.secretaria, t.documento_obtenido, t.telefono, t.observaciones
 				FROM tramites AS t, secretarias AS s
 			WHERE t.id_secretaria = $id_dependencia AND s.id = $id_dependencia AND t.status = 1";
 
